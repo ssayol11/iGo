39dd5590c1b9e451449eb0f5b7f8d521f0e885d0
Roger Bel <83397734+RogerBel@users.noreply.github.com>
2021-05-22 11:57:20 +0200

    Update igo2.py

--- igo2.py
+++ igo2.py
@@ -12,9 +12,6 @@ SIZE = 800
 HIGHWAYS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/1090983a-1c40-4609-8620-14ad49aae3ab/resource/1d6c814c-70ef-4147-aa16-a49ddb952f72/download/transit_relacio_trams.csv'
 CONGESTIONS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/8319c2b1-4c21-4962-9acd-6db4c5ff1148/resource/2d456eb5-4ea6-4f68-9794-2f3f1a58a933/download'
 
-#Highway = collections.namedtuple('Highway', '...') # Tram
-#Congestion = collections.namedtuple('Congestion', '...')
-
 def download_graph (PLACE):
     graph = osmnx.graph_from_place(PLACE, network_type='drive', simplify=True)
     graph = osmnx.utils_graph.get_digraph(graph, weight='length')
