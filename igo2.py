73ba1ffd75d311dee8b8ddff349bec9c9f787f1e
Santi <83207233+ssayol11@users.noreply.github.com>
2021-05-22 17:22:32 +0200

    Update igo2.py

--- igo2.py
+++ igo2.py
@@ -12,16 +12,16 @@ SIZE = 800
 HIGHWAYS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/1090983a-1c40-4609-8620-14ad49aae3ab/resource/1d6c814c-70ef-4147-aa16-a49ddb952f72/download/transit_relacio_trams.csv'
 CONGESTIONS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/8319c2b1-4c21-4962-9acd-6db4c5ff1148/resource/2d456eb5-4ea6-4f68-9794-2f3f1a58a933/download'
 
-def download_graph (PLACE):
+def download_graph(PLACE):
     graph = osmnx.graph_from_place(PLACE, network_type='drive', simplify=True)
     graph = osmnx.utils_graph.get_digraph(graph, weight='length')
     return graph
 
-def save_graph (graph, GRAPH_FILENAME):
+def save_graph(graph, GRAPH_FILENAME):
     with open(GRAPH_FILENAME, 'wb') as file:
         pickle.dump(graph, file)
 
-def load_graph (GRAPH_FILENAME):
+def load_graph(GRAPH_FILENAME):
     with open(GRAPH_FILENAME, 'rb') as file:
         graph = pickle.load(file)
     return graph
@@ -50,7 +50,7 @@ def download_highways_congestions(HIGHWAYS_URL, CONGESTIONS_URL):
 def plot_highways(highways_congestions, file, SIZE):
     bcn_map = StaticMap(SIZE, SIZE)
     for key in highways_congestions:
-        line = Line(highways_congestions.get(key)[1], 'rgb(153,51,255)', 2)
+        line = Line(highways_congestions.get(key)[1], 'rgb(153,51,255)', 3)
         bcn_map.add_line(line)
     image = bcn_map.render()
     image.save(file)
@@ -73,7 +73,7 @@ def main():
         graph = load_graph(GRAPH_FILENAME)
     except:
         graph = download_graph(PLACE)
-        save_graph (graph, GRAPH_FILENAME)
+        save_graph(graph, GRAPH_FILENAME)
 
     #osmnx.plot_graph(graph)
 
