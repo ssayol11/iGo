c2dd4a7612b382f53089594e30b0810eec6e7b9d
Santi <83207233+ssayol11@users.noreply.github.com>
2021-05-26 18:05:42 +0200

    Update igo2.py

--- igo2.py
+++ igo2.py
@@ -1,4 +1,3 @@
-import collections
 import osmnx
 import pickle
 import urllib
@@ -12,9 +11,12 @@ SIZE = 800
 HIGHWAYS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/1090983a-1c40-4609-8620-14ad49aae3ab/resource/1d6c814c-70ef-4147-aa16-a49ddb952f72/download/transit_relacio_trams.csv'
 CONGESTIONS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/8319c2b1-4c21-4962-9acd-6db4c5ff1148/resource/2d456eb5-4ea6-4f68-9794-2f3f1a58a933/download'
 
+#Highway = collections.namedtuple('Highway', '...') # Tram
+#Congestion = collections.namedtuple('Congestion', '...')
+
 def download_graph(PLACE):
     graph = osmnx.graph_from_place(PLACE, network_type='drive', simplify=True)
-    graph = osmnx.utils_graph.get_digraph(graph, weight='length')
+    #graph = osmnx.utils_graph.get_digraph(graph, weight='length')
     return graph
 
 def save_graph(graph, GRAPH_FILENAME):
@@ -27,9 +29,14 @@ def load_graph(GRAPH_FILENAME):
     return graph
 
 def to_pairs(coordinates):
-    coords = coordinates.split(',', 4)
-    return list([(float(coords[0]), float(coords[1])), (float(coords[2]), float(coords[3]))])
-
+    coords = coordinates.split(',')
+    pairs = []
+    i = 0
+    while i < len(coords):
+        pairs.append((float(coords[i]), float(coords[i + 1])))
+        i += 2
+    return pairs
+    
 def download_highways_congestions(HIGHWAYS_URL, CONGESTIONS_URL):
     highways_congestions = {}
     with urllib.request.urlopen(HIGHWAYS_URL) as response:
@@ -50,22 +57,35 @@ def download_highways_congestions(HIGHWAYS_URL, CONGESTIONS_URL):
 def plot_highways(highways_congestions, file, SIZE):
     bcn_map = StaticMap(SIZE, SIZE)
     for key in highways_congestions:
-        line = Line(highways_congestions.get(key)[1], 'rgb(153,51,255)', 3)
+        line = Line(highways_congestions.get(key)[1], 'rgb(153,51,255)', 2)
         bcn_map.add_line(line)
     image = bcn_map.render()
     image.save(file)
     
-def choose_colour(n):   
-    colours = ['rgb(128,128,128)', 'blue', 'green', 'rgb(255,255,0)', 'rgb(255,128,0)', 'red', 'rgb(0,0,0)']
-    return colours[n]
+def choose_color(n):   
+    colors = ['rgb(128,128,128)', 'blue', 'green', 'rgb(255,255,0)', 'rgb(255,128,0)', 'red', 'rgb(0,0,0)']
+    return colors[n]
 
 def plot_congestions(highways_congestions, file, SIZE):
     bcn_map = StaticMap(SIZE, SIZE)
     for key in highways_congestions:
-        line = Line(highways_congestions.get(key)[1], choose_colour(int(highways_congestions.get(key)[2])), 4)
+        line = Line(highways_congestions.get(key)[1], choose_color(int(highways_congestions.get(key)[2])), 2)
         bcn_map.add_line(line)
     image = bcn_map.render()
     image.save(file)
+    
+def build_igraph(graph, highways_congestions):
+    graph = osmnx.add_edge_speeds(graph)
+    for key in highways_congestions:
+        if highways_congestions.get(key)[2] != 0:
+            for i in range(len(highways_congestions.get(key)[1])):
+                start_coord = highways_congestions.get(key)[1][i]
+                finish_coord = highways_congestions.get(key)[1][i + 1]
+                
+                start_node = osmnx.distance.nearest_nodes(graph, start_coord[0], start_coord[1])
+                finish_node = osmnx.distance.nearest_nodes(graph, finish_coord[0], finish_coord[1])
+                
+                print(key, start_node, finish_node)
 
 def main():
     # load/download graph (using cache)
@@ -76,9 +96,14 @@ def main():
         save_graph(graph, GRAPH_FILENAME)
 
     #osmnx.plot_graph(graph)
+    
+    #a = osmnx.basic_stats(graph)
+    #print(a)
 
     highways_and_congestions = download_highways_congestions(HIGHWAYS_URL, CONGESTIONS_URL)
     plot_highways(highways_and_congestions, 'highways.png', SIZE)
     plot_congestions(highways_and_congestions, 'congestions.png', SIZE)
-
+    
+    build_igraph(graph, highways_and_congestions)
+    
 main()
